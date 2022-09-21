74d712ce2a5b19dc1c14d095c1b7dd5678e742cc
ibrah <ibrahim.kubanov@gmail.com>
2022-09-22 00:17:46 +0300

    added login layout template with cakcha

--- app/src/main/java/com/example/basadannih/MainActivity.java
+++ app/src/main/java/com/example/basadannih/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.basadannih;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView captcha = findViewById(R.id.captcha);
+        captcha.setText(generateCaptcha());
+    }
+
+    public static String generateCaptcha() {
+        String captcha = "";
+        try {
+            String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for (int i = 0; i < 5; i++) {
+                captcha += letters.charAt((int) (Math.random() * letters.length()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return captcha;
+    }
+
+    public void onGenerateClick(View view) {
+        TextView captcha = findViewById(R.id.captcha);
+        captcha.setText(generateCaptcha());
     }
 }
