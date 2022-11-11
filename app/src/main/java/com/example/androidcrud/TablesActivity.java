88524cb86f1d81990a462847dd282127c4125e15
Ismas2003 <Rjym2014>
2022-11-11 22:59:15 +0300

    Добавил меню с таблицами

--- app/src/main/java/com/example/androidcrud/TablesActivity.java
+++ app/src/main/java/com/example/androidcrud/TablesActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidcrud;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,5 +19,40 @@ public class TablesActivity extends AppCompatActivity {
         } else {
             setTitle("User");
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tables_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView noneTextView = findViewById(R.id.noneTextView);
+        noneTextView.setVisibility(TextView.INVISIBLE);
+        switch(id){
+            case R.id.doctorsMenuItem:
+                setTitle("Doctors");
+                return true;
+            case R.id.operationsMenuItem:
+                setTitle("Operations");
+                return true;
+            case R.id.operationsTypesMenuItem:
+                setTitle("Operations Types");
+                return true;
+            case R.id.patientsMenuItem:
+                setTitle("Patients");
+                return true;
+            case R.id.wardsMenuItem:
+                setTitle("Wards");
+                return true;
+            case R.id.noneMenuItem:
+                setTitle("None");
+                noneTextView.setVisibility(TextView.VISIBLE);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
