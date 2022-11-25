b479fce13dfd85257201982725c92bf0f78a1bd9
Ismas2003 <Rjym2014>
2022-11-25 18:36:49 +0300

    Добавил рабочий OnClickListener для RecyclerView TODO: При переходе на окно добавления всегда отображаются данные первого врача

--- app/src/main/java/com/example/androidcrud/addEditActivities/DoctorsAeActivity.java
+++ app/src/main/java/com/example/androidcrud/addEditActivities/DoctorsAeActivity.java
@@ -7,8 +7,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.androidcrud.MainActivity;
 import com.example.androidcrud.R;
 import com.example.androidcrud.TablesActivity;
+import com.example.androidcrud.tables.Doctors;
+
+import java.util.List;
 
 
 public class DoctorsAeActivity extends AppCompatActivity {
@@ -18,6 +22,24 @@ public class DoctorsAeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctors_ae);
+
+        int id = getIntent().getExtras().getInt("id");
+        List<Doctors> doctors = MainActivity.db.doctorsDao().getAll();
+        Doctors doctor = doctors.get(id);
+
+        firstName = findViewById(R.id.input_first_name);
+        lastName = findViewById(R.id.input_last_name);
+        patronymic = findViewById(R.id.input_patronymic);
+        experience = findViewById(R.id.input_experience);
+        login = findViewById(R.id.input_login);
+        password = findViewById(R.id.input_password);
+
+        firstName.setText(doctor.firstName);
+        lastName.setText(doctor.lastName);
+        patronymic.setText(doctor.patronymic);
+        experience.setText(String.valueOf(doctor.experience));
+        login.setText(doctor.login);
+        password.setText(doctor.password);
     }
 
     public void onBackToTablesClick(View view) {
